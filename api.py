116703cb0a5e2b483dcf7e478e2f366b827a2c48
石文文 <shiwenwendevelop@163.com>
2018-10-15 14:15:42 +0800

    api 模块

---
+++ api.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+# Author: ShiWenWen
+from  flask import Flask, g
+from redisdb import RedisClient
+
+
+__all__ = ['app']
+app = Flask(__name__)
+
+def get_conn():
+    if not hasattr(g, 'redis'):
+        g.redis = RedisClient()
+    return g.redis
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to Proxy Pool System</h2>'
+
+@app.route('/random')
+def get_proxy():
+    """
+    获取随机可用代理
+    :return: 随机代理
+    """
+    return get_conn().random()
+
+@app.route('/count')
+def get_counts():
+    """
+    获取代理池总量
+    :return: 代理池总量
+    """
+    return str(get_conn().count())
+
+if __name__ == '__main__':
+    app.run()
+
+
+
