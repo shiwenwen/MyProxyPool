4738dce8d3916693003e73f8c4fd58c6aa1a7ab5
石文文 <shiwenwendevelop@163.com>
2018-10-15 14:16:06 +0800

    api 模块

--- api.py
+++ api.py
@@ -1,20 +1,23 @@
 # -*- coding:utf-8 -*-
 # Author: ShiWenWen
-from  flask import Flask, g
-from redisdb import RedisClient
+from flask import Flask, g
+from redisdb import RedisCli
 
 
 __all__ = ['app']
 app = Flask(__name__)
 
+
 def get_conn():
     if not hasattr(g, 'redis'):
-        g.redis = RedisClient()
+        g.redis = RedisCli()
     return g.redis
+
 
 @app.route('/')
 def index():
-    return '<h2>Welcome to Proxy Pool System</h2>'
+    return '<h1>Welcome to Proxy Pool System</h1>'
+
 
 @app.route('/random')
 def get_proxy():
@@ -24,6 +27,7 @@ def get_proxy():
     """
     return get_conn().random()
 
+
 @app.route('/count')
 def get_counts():
     """
@@ -31,6 +35,7 @@ def get_counts():
     :return: 代理池总量
     """
     return str(get_conn().count())
+
 
 if __name__ == '__main__':
     app.run()
